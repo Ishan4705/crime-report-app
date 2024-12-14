176a4ec1f37bc5e7f2ebd6714063677ab54f1d85
Ishan Mittal <ishanm.2205@gmail.com>
2024-12-14 13:57:19 +0530

    Navbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
-// import Navbar from './components/Navbar'
-// import Providers from './components/Providers'
+import Navbar from '@/components/Navbar'
+
 
 const inter =Inter({subsets:['latin']});
 
 export const metadata: Metadata ={
-  title:"SafeReport - Anonymously Report Crimes",
+  title:"NukaAlert - Anonymously Report Crimes",
   description:"Securely and anonymously report crimes to law enforcement agencies",
 }
 
@@ -20,7 +20,7 @@ export default function RootLayout({children}: {children: React.ReactNode;}) {
           <div className="absolute inset-0 h-full bg-[radial-gradient(circle_at_center,rgba(56,189,248,0.03),transparent_50%)]" />
           <div className="absolute inset-0 h-full bg-[radial-gradient(circle_at_center,rgba(14,165,233,0.04),transparent_70%)]" />
         </div>
-        {/* <Navbar /> */}
+        <Navbar />
           <main className="pt-16">
             {children}
             {/* <Providers>{children}</Providers> */}
